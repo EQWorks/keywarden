a64dc9e6a7486a05fb1dc7d1c9fea6fbf15b01ba
woozyking <runzhou.li@gmail.com>
2019-05-06 15:34:30 -0400

    add readme.io JWT login

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const api = express.Router()
 
 const rootRouter = require('./root')
 const usersRouter = require('./users')
+const extRouter = require('./external')
 
 api.use('/', rootRouter)
 api.use('/users', usersRouter)
+api.use('/external', extRouter)
 
 module.exports = api
