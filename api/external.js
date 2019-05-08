f2f853562af17967b6dcf9e4adee07dbe022f819
woozyking <runzhou.li@gmail.com>
2019-05-08 17:05:05 -0400

    remove email workflow readme JWT login, add isReadOnly for readme access control

--- api/external.js
+++ api/external.js
@@ -18,35 +18,10 @@ router.get('/readme', (req, res, next) => {
     if (!client) {
       return res.sendStatus(403)
     }
-    const user = { email, name: email, prefix, ...client }
+    // `isReadOnly` is used for readme.io's access control
+    const user = { email, name: email, prefix, isReadOnly: true, ...client }
     const link = `https://docs.locus.place/v1.0?auth_token=${signJWT(user, README_LOCUS_SECRET)}`
     return res.status(200).json({ message: 'Login link generated', link })
-  }).catch(next)
-})
-
-// GET /readme/login through email workflow
-router.get('/readme/login', (req, res, next) => {
-  let { email, user } = req.query
-  email = email || user
-  const selects = ['prefix', 'client']
-  selectUser({ email, selects }).then(({ user: { client, prefix } }) => {
-    const user = { email, name: email, prefix, ...client }
-    const link = `http://docs.locus.place/v1.0?auth_token=${signJWT(user, README_LOCUS_SECRET)}`
-    const company = 'EQ Works'
-    const product = 'LOCUS Developer Hub'
-    const message = {
-      from: 'dev@eqworks.com',
-      to: email,
-      subject: `${product} (${company}) Login`,
-      text: magicLinkText({ link, company, product }),
-      html: magicLinkHTML({ link, company, product }),
-    }
-    return sendMail(message)
-  }).then(() => {
-    return res.status(200).json({
-      message: `Login magic link sent to ${email} through email`,
-      email,
-    })
   }).catch(next)
 })
 
