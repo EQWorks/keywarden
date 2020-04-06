ff9b803e2c188756714cfd6925519dce5436eecf
Maliha Hossain <malu.hoss@gmail.com>
2020-04-06 14:28:04 -0400

    Users: Created hardcoded passcode for app reviewer user - check user prefixes and if app reviewer, no OTP generated

--- modules/auth.js
+++ modules/auth.js
@@ -95,8 +95,12 @@ const loginUser = async ({ user, redirect, zone='utc', product='ATOM' }) => {
   let { sender, company } = rows[0] || {}
   sender = sender || 'dev@eqworks.com'
   company = company || 'EQ Works'
+
+  const userExists = await getUserInfo({ email: user })
+  const isAppReviewer = userExists.prefix === 'appreviewer'
   // generate and update user OTP, get TTL
-  const otp = _genOTP()
+  const otp = (!isAppReviewer) ? _genOTP() : '000000'
+  
   let ttl = await _updateOTP({ email: user, otp })
   // localize TTL
   ttl = moment.tz(ttl, zone).format('LLLL z')
