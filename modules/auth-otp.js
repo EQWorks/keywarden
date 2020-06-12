4f9bd67a021d2832b7dfff28956a78298a1ef431
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro.local>
2020-06-12 10:50:01 -0400

    OTP - Switch from TOTP to TUK

--- modules/auth-otp.js
+++ modules/auth-otp.js
@@ -4,109 +4,79 @@ const { AuthorizationError } = require('./errors')
 
 const redisClient = new Redis(process.env.REDIS_URI)
 
-// updates redis TOTP meta atomically
-redisClient.defineCommand('updateCurrentTOTPMeta', {
+// gets key or sets value if does not exist + sets/resets ttl (when ttl args provided)
+redisClient.defineCommand('getOrSet', {
   numberOfKeys: 1,
   lua: `
-    -- get time interval and otp index
-    local currentInterval = ARGV[1]
-    local currentIndex = ARGV[2]
-    local listLength = redis.call('LLEN', KEYS[1])
+    -- look up ttl to determine existence + if within min ttl
+    local ttl = redis.call('PTTL', KEYS[1])
 
-    if listLength == 2 then
-      currentInterval = redis.call('LINDEX', KEYS[1], 0)
-      currentIndex = redis.call('LINDEX', KEYS[1], 1)
+    if ARGV[2] ~= nil and ARGV[2] ~= "" and ARGV[3] ~= nil and ARGV[3] ~= "" and ttl < tonumber(ARGV[2]) then
+      -- set key if ttl args have been provided and ttl is less than minTTL (including evergreen or nil key)
+      ttl = tonumber(ARGV[3])
+      redis.call('SET', KEYS[1], ARGV[1], 'PX', ttl)
+      return {ARGV[1], ttl}
+
+    elseif ttl == -2 then
+      -- set ttl to -1 (no expiry) if new key with no ttl args
+      ttl = -1
+      redis.call('SET', KEYS[1], ARGV[1])
+      return {ARGV[1], ttl}
     end
 
-    -- return false if not expected values
-    if currentInterval ~= ARGV[1] or currentIndex ~= ARGV[2] then
-      return false
-    end
-
-    -- update or set otp interval + index
-    if listLength == 2 then
-      redis.call('LSET', KEYS[1], 0, ARGV[3])
-      redis.call('LSET', KEYS[1], 1, ARGV[4])
-    else
-      redis.call('RPUSH', KEYS[1], ARGV[3])
-      redis.call('RPUSH', KEYS[1], ARGV[4])
-    end
-
-    -- set/update ttl
-    if ARGV[5] ~= nil and ARGV[5] ~= "" then
-      redis.call('PEXPIREAT', KEYS[1], ARGV[5])
-    end
-
-    return true
-  `
-})
-
-// get TOTP meta from redis
-redisClient.defineCommand('getCurrentTOTPMeta', {
-  numberOfKeys: 1,
-  lua: `
-    local interval = 0
-    local index = 0
-
-    if redis.call('LLEN', KEYS[1]) == 2 then
-      interval = redis.call('LINDEX', KEYS[1], 0)
-      index = redis.call('LINDEX', KEYS[1], 1)
-    end
-
-    return {interval, index}
+    -- otherwise return key
+    return {redis.call('GET', KEYS[1]), ttl}
   `
 })
 
 /**
- * Updates cached TOTP meta data atomically
+ * Sets the user's TUK (Temporary User Key) to the value provided if no TUK can be
+ * found in cache or if the existing TUK's TTL is less than the minimum TTL.
+ * Returns cached value along with TTL
  * @param {Object} [options]
  * @param {string} options.email
- * @param {number} [options.currentInterval=0] Time interval for the last issued TOTP
- * @param {number} [options.currentIndex=0] Index with time interval for the last issues TOTP
- * @param {number} options.nextInterval Time interval to update to
- * @param {number} [options.nextIndex=0] Index to update to
- * @param {number} [options.ttl] Unix epoch time (in milliseconds) after which the cache will clear
- * @return {Promise<undefined>}
- * @throws {AuthorizationError} Throws if failure
+ * @param {string} options.set Value to set the TUK to when the cache is empty
+ * @param {number} [options.minTTL] Minimum TTL (in milliseconds) required to keep existing TUK
+ * @param {number} [options.resetTTL] TTL (in milliseconds) when TUK is replaced
+ * @return {Promise<{tuk: string, ttl: number}>} TTL expressed as time since unix epoch in
+ * milliseconds or -1 if evergreen
  */
-const updateRedeemableTOTPMeta = async ({ email, currentInterval = 0, currentIndex = 0, nextInterval, nextIndex = 0, ttl}) => {
-  const success = await redisClient.updateCurrentTOTPMeta(`keywarden-totp-${email}`, currentInterval, currentIndex, nextInterval, nextIndex, ttl)
-  if (!success) {
-    throw new AuthorizationError('Failed to update Redis cache for OTP.')
-  }
+const getOrSetTUK = async ({ email, set, minTTL, resetTTL}) => {
+  const [tuk, ttl] = await redisClient.getOrSet(`keywarden-otp-${email}`, set, minTTL, resetTTL)
+  return { tuk, ttl: ttl > 0 ? Date.now() + ttl : ttl }
 }
 
 /**
- * Retrieves cached TOTP meta data for TOTP currently redeemable (i.e. last issued within TTL)
+ * Retrieves the user's TUK (Temporary User Key) from cache
  * @param {string} email
- * @return {Promise<{interval: number, index: number}>} { interval: 0, index: 0 } if no TOTP issued and within TTL
+ * @return {string|undefined} TUK, undefined if none cached
  */
-const getRedeemableTOTPMeta = async (email) => {
-  const [interval, index] = await redisClient.getCurrentTOTPMeta(`keywarden-totp-${email}`)
-  return { interval: parseInt(interval, 10), index: parseInt(index, 10) }
+const getTUK = async (email) => {
+  const tuk = await redisClient.get(`keywarden-otp-${email}`)
+  return tuk || undefined
 }
 
 /**
- * Returns the current unix time interval according to intervalLength
- * @param {number} [intervalLength=1000] Time interval length (in milliseconds)
- * @return {number} Current unix time interval
+ * Deletes the user's TUK (Temporary User Key) from cache
+ * @param {string} email
+ * @return {Promise<boolean>} true if the key was deleted, false otherwise
  */
-const getTimeInterval = (intervalLength = 1000) => {
-  return (Date.now() / intervalLength) >> 0
+const clearTUK = async (email) => {
+  const deleted = await redisClient.del(`keywarden-otp-${email}`)
+  return deleted == 1
 }
 
 /**
- * Generates a TOTP
+ * Generates a OTP in a deterministic way
  * @param {Object} [options]
  * @param {string} options.email
  * @param {string} options.secret
- * @param {number} options.interval Unix time interval index
- * @param {number} [options.length=6] Length of the TOTP string (max 32, multiple of 2)
- * @param {number} [options.index=0] TOTP index within the time interval
- * @return {string} TOTP
+ * @param {string} options.tuk Temporary user key
+ * @param {number} [options.length=6] Length of the OTP string (max 32, multiple of 2)
+ * @return {string} OTP
  */
-const genTOTP = ({ email, secret, interval, length = 6, index = 0 }) => {
-  return crypto.createHmac('sha256', secret + interval + index)
+const genOTP = ({ email, secret, tuk, length = 6 }) => {
+  return crypto.createHmac('sha256', secret + tuk)
     .update(email, 'utf8')
     .digest()
     .swap64()
@@ -116,52 +86,47 @@ const genTOTP = ({ email, secret, interval, length = 6, index = 0 }) => {
 }
 
 /**
- * Generates a TOTP with a TTL of intervalLength
+ * Generates a OTP with a TTL in the range [minTTL, resetTTL] or no TTL (evergreen) if
+ * these arguments are not supplied
  * @param {Object} [options]
  * @param {string} options.email
  * @param {string} options.secret
- * @param {number} [options.length=6] Length of the TOTP string (max 32, multiple of 2)
- * @param {number} [options.intervalLength=1000*60*5] Time interval length (in milliseconds)
- * @return {Promise<{ otp: string, ttl: number}>} TOTP object
+ * @param {number} [options.length=6] Length of the OTP string (max 32, multiple of 2)
+ * @param {number} [options.minTTL] Min TTL (in milliseconds) left to reuse previously issued OTP
+ * @param {number} [options.resetTTL] TTL (in milliseconds) for newly issued OTP
+ * @return {Promise<{otp: string, ttl: number}>} OTP object
  * @throws {AuthorizationError} Throws if failure
  */
-const claimTOTP = async ({ email, secret, length = 6, intervalLength = 1000 * 60 * 5 }) => {
-  const { interval: currentInterval, index: currentIndex } = await getRedeemableTOTPMeta(email)
-  const nextInterval = getTimeInterval(intervalLength) + 1
-  const nextIndex = currentInterval === nextInterval ? currentIndex : 0
-
-  const otp = genTOTP({ email, secret, interval: nextInterval, length, index: nextIndex })
-  const ttl = (nextInterval + 1) * intervalLength - 1
-
-  if (currentInterval !== nextInterval || currentIndex !== nextIndex) {
-    await updateRedeemableTOTPMeta({ email, currentInterval, currentIndex, nextInterval, nextIndex, ttl })
-  }
-
+const claimOTP = async ({ email, secret, length = 6, minTTL, resetTTL }) => {
+  const set = crypto.randomBytes(10).toString('hex')
+  const { tuk, ttl } = await getOrSetTUK({ email, set, minTTL, resetTTL })
+  const otp = genOTP({ email, secret, tuk, length })
   return { otp, ttl }
 }
 
 /**
- * Validates the supplied TOTP against TOTP in cache and updates cache value to next index
+ * Validates the supplied OTP using the cached TUK
  * @param {Object} [options]
- * @param {string} options.otp TOTP for which validation is sought
+ * @param {string} options.otp OTP for which validation is sought
  * @param {string} options.email
  * @param {string} options.secret
- * @param {number} [options.length=6] Length of the TOTP string (max 32, multiple of 2)
+ * @param {number} [options.length=6] Length of the OTP string (max 32, multiple of 2)
  * @return {Promise<undefined>}
  * @throws {AuthorizationError} Throws if failure
  */
-const redeemTOTP = async ({ otp, email, secret, length = 6 }) => {
-  const { interval, index } = await getRedeemableTOTPMeta(email)
-  if (!interval) {
-    throw new AuthorizationError(`No TOTP currently valid for user ${email}`)
+const redeemOTP = async ({ otp, email, secret, length = 6 }) => {
+  const tuk = await getTUK(email)
+  // if no TUK, then all OTP have been redeemed or have expired
+  if (!tuk) {
+    throw new AuthorizationError(`No OTP to redeem for user ${email}`)
   }
-  if (genTOTP({ email, secret, interval, length, index }) !== otp.trim().toUpperCase()) {
-    throw new AuthorizationError(`Invalid TOTP for user ${email}`)
+  if (genOTP({ email, secret, tuk, length }) !== otp.trim().toUpperCase()) {
+    throw new AuthorizationError(`Invalid OTP for user ${email}`)
   }
-  await updateRedeemableTOTPMeta({ email, currentInterval: interval, currentIndex: index, nextInterval: interval, nextIndex: index + 1 })
+  await clearTUK(email)
 }
 
 module.exports = {
-  claimTOTP,
-  redeemTOTP
+  claimOTP,
+  redeemOTP
 }
